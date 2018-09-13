397c9b9b131efbb1d1ad8a650fbb9c6aedab0ac6
palty8 <reco4life@gmail.com>
2018-09-13 13:03:24 +0700

    исправил недочеты

--- js/script.js
+++ js/script.js
@@ -73,19 +73,19 @@ const mapClose = document.querySelector(".modal-map .modal-close");
 
 mapLink.addEventListener("click", function (evt) {
   evt.preventDefault();
-  mapPopup.classList.add("modal-show");
+  mapPopup.classList.add("modal-map-show");
 });
 
 mapClose.addEventListener("click", function (evt) {
   evt.preventDefault();
-  mapPopup.classList.remove("modal-show");
+  mapPopup.classList.remove("modal-map-show");
 });
 
 window.addEventListener("keydown", function (evt) {
   if (evt.keyCode === 27) {
-    if (mapPopup.classList.contains("modal-show")) {
+    if (mapPopup.classList.contains("modal-map-show")) {
       evt.preventDefault();
-      mapPopup.classList.remove("modal-show");
+      mapPopup.classList.remove("modal-map-show");
     }
   }
 });
